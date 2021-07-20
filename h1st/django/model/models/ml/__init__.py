e0651904749316d4c7b5d9bb4c415fef3e6e0bd1
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-07-19 23:59:26 -0700

    update h1st.django.model.models.ml

--- h1st/django/model/models/ml/__init__.py
+++ h1st/django/model/models/ml/__init__.py
@@ -6,3 +6,12 @@ from .tf import TFModel, H1stTFModel
 from .keras import KerasModel, H1stKerasModel
 
 from .torch import TorchModel, H1stTorchModel
+
+
+__all__ = [
+    'MLModel', 'H1stMLModel',
+    'SKLModel', 'H1stSKLModel',
+    'TFModel', 'H1stTFModel',
+    'KerasModel', 'H1stKerasModel',
+    'TorchModel', 'H1stTorchModel'
+]

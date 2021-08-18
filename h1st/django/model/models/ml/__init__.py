51794a723b69ba2946debbcaafdcd9eee4327944
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-08-18 16:01:39 -0700

    refactor h1st.django.model.models.ml

--- h1st/django/model/models/ml/__init__.py
+++ h1st/django/model/models/ml/__init__.py
@@ -1,3 +1,15 @@
+__all__ = (
+    'MLModel', 'H1stMLModel',
+
+    'SKLModel', 'H1stSKLModel',
+
+    'TFModel', 'H1stTFModel',
+    'KerasModel', 'H1stKerasModel',
+
+    'TorchModel', 'H1stTorchModel'
+)
+
+
 from .base import MLModel, H1stMLModel
 
 from .skl import SKLModel, H1stSKLModel
@@ -6,12 +18,3 @@ from .tf import TFModel, H1stTFModel
 from .keras import KerasModel, H1stKerasModel
 
 from .torch import TorchModel, H1stTorchModel
-
-
-__all__ = [
-    'MLModel', 'H1stMLModel',
-    'SKLModel', 'H1stSKLModel',
-    'TFModel', 'H1stTFModel',
-    'KerasModel', 'H1stKerasModel',
-    'TorchModel', 'H1stTorchModel'
-]

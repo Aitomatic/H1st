8229f815e316373c5dd93f7f96157f16175b9b3f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-09-29 23:59:49 -0700

    update h1st.django.model.api

--- h1st/django/model/api/__init__.py
+++ h1st/django/model/api/__init__.py
@@ -6,3 +6,11 @@ from ..models import (
 
     Workflow, H1stWorkflow
 )
+
+
+__all__ = ('Model', 'H1stModel',
+           'SKLModel', 'H1stSKLModel',
+           'TFModel', 'H1stTFModel',
+           'KerasModel', 'H1stKerasModel',
+
+           'Workflow', 'H1stWorkflow')

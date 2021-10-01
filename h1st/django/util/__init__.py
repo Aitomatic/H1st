0af192e21fb2432f150b4f02d1f4b1577bdbdb55
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-09-30 23:53:24 -0700

    add h1st.django.util.import_obj

--- h1st/django/util/__init__.py
+++ h1st/django/util/__init__.py
@@ -1,7 +1,13 @@
+from importlib import import_module
+
+
 PGSQL_IDENTIFIER_MAX_LEN = 63
 
 
 def dir_path_with_slash(path: str) -> str:
-    return path \
-        if path.endswith('/') \
-      else f'{path}/'
+    return path if path.endswith('/') else f'{path}/'
+
+
+def import_obj(module_and_qualname: str):
+    module_name, qualname = module_and_qualname.rsplit(sep='.', maxsplit=1)
+    return getattr(import_module(module_name), qualname)

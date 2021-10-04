051d0a426d8fa9a3166c5ab8c7ae2ebd2d92170b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-04 01:14:02 -0700

    update h1st.django.model.views Gradio UI

--- h1st/django/model/urls.py
+++ h1st/django/model/urls.py
@@ -3,7 +3,7 @@ from django.urls.conf import include, path
 from rest_framework.routers import DefaultRouter
 
 from .api.rest.views import H1stModelViewSet, ModelExecAPIView, TestAPIView
-from .views import model_exec_on_json_input_data, gradio_ui_view
+from .views import exec_on_json_input_data, launch_gradio_ui
 
 
 CORE_REST_API_ROUTER = DefaultRouter(trailing_slash=False)
@@ -18,14 +18,14 @@ urlpatterns = [
     path(route='',
          view=include(CORE_REST_API_ROUTER.urls)),
 
-    path(route='<str:model_uuid>/gradio/',
-         view=gradio_ui_view),
+    path(route='<str:model_name_or_uuid>/gradio/',
+         view=launch_gradio_ui),
 
     path(route='exec/',
          view=ModelExecAPIView.as_view()),
 
-    path(route='<str:model_uuid>/<str:json_input_data>/',
-         view=model_exec_on_json_input_data),
+    path(route='<str:model_name_or_uuid>/<str:json_input_data>/',
+         view=exec_on_json_input_data),
 
     path(route='test/',
          view=TestAPIView.as_view())

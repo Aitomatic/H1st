76b9c0a877aff1432b315c82427dd322917f57ed
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-03 23:38:24 -0700

    update h1st.django.model.models.ml.keras.pre_trained.image_classification Gradio UI

--- h1st/django/model/models/ml/keras/pre_trained/image_classification.py
+++ h1st/django/model/models/ml/keras/pre_trained/image_classification.py
@@ -1,7 +1,8 @@
-from typing import Sequence, Tuple, Union
+from typing import Sequence, Union
 
 from django.db.models.fields import CharField
 
+from gradio.interface import Interface
 from gradio.inputs import Image as ImageInputComponent
 from gradio.outputs import Label as LabelOutputComponent
 
@@ -121,21 +122,156 @@ class PreTrainedKerasImageNetClassifier(H1stPyLoadablePreTrainedMLModel):
         return (decoded_preds[0] if single_img else decoded_preds)
 
     @property
-    def gradio_ui(self) -> Tuple[ImageInputComponent, LabelOutputComponent]:
+    def gradio_ui(self) -> Interface:
         img_dim_size = self.img_dim_size
 
-        return (ImageInputComponent(shape=(img_dim_size, img_dim_size),
-                                    image_mode='RGB',
-                                    invert_colors=False,
-                                    source='upload',
-                                    tool='editor',
-                                    type='numpy',
-                                    label='Upload an Image to Classify',
-                                    optional=False),
+        return Interface(
+            fn=self.predict,
+            # (Callable) - the function to wrap an interface around.
 
-                LabelOutputComponent(num_top_classes=5,
-                                     type='auto',
-                                     label=f'Predictions by {self}'))
+            inputs=ImageInputComponent(shape=(img_dim_size, img_dim_size),
+                                       image_mode='RGB',
+                                       invert_colors=False,
+                                       source='upload',
+                                       tool='editor',
+                                       type='numpy',
+                                       label='Upload an Image to Classify',
+                                       optional=False),
+            # (Union[str, List[Union[str, InputComponent]]]) -
+            # a single Gradio input component,
+            # or list of Gradio input components.
+            # Components can either be passed as instantiated objects,
+            # or referred to by their string shortcuts.
+            # The number of input components should match
+            # the number of parameters in fn.
+
+            outputs=LabelOutputComponent(num_top_classes=5,
+                                         type='auto',
+                                         label='Likely ImageNet Classes'),
+            # (Union[str, List[Union[str, OutputComponent]]]) -
+            # a single Gradio output component,
+            # or list of Gradio output components.
+            # Components can either be passed as instantiated objects,
+            # or referred to by their string shortcuts.
+            # The number of output components should match
+            # the number of values returned by fn.
+
+            verbose=True,
+            # (bool) - whether to print detailed information during launch.
+
+            examples=None,
+            # (Union[List[List[Any]], str]) - sample inputs for the function;
+            # if provided, appears below the UI components and can be used
+            # to populate the interface.
+            # Should be nested list, in which the outer list consists of
+            # samples and each inner list consists of an input
+            # corresponding to each input component.
+            # A string path to a directory of examples can also be provided.
+            # If there are multiple input components and a directory
+            # is provided, a log.csv file must be present in the directory
+            # to link corresponding inputs.
+
+            examples_per_page=10,
+            # (int) - If examples are provided, how many to display per page.
+
+            live=False,
+            # (bool) - should the interface automatically reload on change?
+
+            layout='unaligned',
+            # (str) - Layout of input and output panels.
+            # - "horizontal" arranges them as two columns of equal height,
+            # - "unaligned" arranges them as two columns of unequal height, and
+            # - "vertical" arranges them vertically.
+
+            show_input=True,
+            show_output=True,
+
+            capture_session=False,
+            # (bool) - if True, captures the default graph and session
+            # (needed for Tensorflow 1.x)
+
+            interpretation='default',
+            # (Union[Callable, str]) - function that provides interpretation
+            # explaining prediction output.
+            # Pass "default" to use built-in interpreter.
+
+            num_shap=2.0,
+            # (float) - a multiplier that determines how many examples
+            # are computed for shap-based interpretation.
+            # Increasing this value will increase shap runtime,
+            # but improve results.
+
+            theme='default',
+            # (str) - Theme to use - one of
+            # - "default",
+            # - "compact",
+            # - "huggingface", or
+            # - "darkhuggingface".
+
+            repeat_outputs_per_model=True,
+
+            title=type(self).__qualname__,
+            # (str) - a title for the interface;
+            # if provided, appears above the input and output components.
+
+            description=self.name,
+            # (str) - a description for the interface;
+            # if provided, appears above the input and output components.
+
+            article=None,
+            # (str) - an expanded article explaining the interface;
+            # if provided, appears below the input and output components.
+            # Accepts Markdown and HTML content.
+
+            thumbnail=None,
+            # (str) - path to image or src to use as display picture for models
+            # listed in gradio.app/hub
+
+            css=None,
+            # (str) - custom css or path to custom css file
+            # to use with interface.
+
+            server_port=None,
+            # (int) - will start gradio app on this port (if available)
+
+            # server_name=networking.LOCALHOST_NAME,
+            # (str) - to make app accessible on local network set to "0.0.0.0".
+
+            height=500,
+            width=900,
+
+            allow_screenshot=True,
+            # (bool) - if False, users will not see a button
+            # to take a screenshot of the interface.
+
+            allow_flagging=True,
+            # (bool) - if False, users will not see a button
+            # to flag an input and output.
+
+            flagging_options=None,
+            # (List[str]) - if not None, provides options a user must select
+            # when flagging.
+
+            encrypt=False,
+            # (bool) - If True, flagged data will be encrypted
+            # by key provided by creator at launch
+
+            show_tips=True,
+            # (bool) - if True, will occasionally show tips
+            # about new Gradio features
+
+            embedding=None,
+
+            flagging_dir='flagged',
+            # (str) - what to name the dir where flagged data is stored.
+
+            analytics_enabled=True,
+
+            enable_queue=False,
+            # (bool) - if True, inference requests will be served through
+            # a queue instead of with parallel threads.
+            # Required for longer inference times (> 1min) to prevent timeout.
+        )
 
 
 # alias

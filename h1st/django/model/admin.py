2f772941336ed26ca310fdfee32e7c3734b55ec2
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-05 00:32:45 -0700

    add h1st.django.model.admin for Google Cloud Translation models

--- h1st/django/model/admin.py
+++ h1st/django/model/admin.py
@@ -5,8 +5,44 @@ from django.contrib.admin.sites import site
 from silk.profiling.profiler import silk_profile
 
 from .models import (
+    GoogleCloudTranslationServiceModel, GoogleTranslateServiceModel,
     PreTrainedKerasImageNetClassifier,
 )
+
+
+@register(GoogleCloudTranslationServiceModel, site=site)
+class GoogleCloudTranslationServiceModelAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=(f'{__module__}: '
+              f'{GoogleCloudTranslationServiceModel._meta.verbose_name}'))
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=(f'{__module__}: '
+              f'{GoogleCloudTranslationServiceModel._meta.verbose_name_plural}'
+              ))
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
+
+
+@register(GoogleTranslateServiceModel, site=site)
+class GoogleTranslateServiceModelAdmin(ModelAdmin):
+    show_full_result_count = False
+
+    @silk_profile(
+        name=(f'{__module__}: '
+              f'{GoogleTranslateServiceModel._meta.verbose_name}'))
+    def changeform_view(self, *args, **kwargs):
+        return super().changeform_view(*args, **kwargs)
+
+    @silk_profile(
+        name=(f'{__module__}: '
+              f'{GoogleTranslateServiceModel._meta.verbose_name_plural}'))
+    def changelist_view(self, *args, **kwargs):
+        return super().changelist_view(*args, **kwargs)
 
 
 @register(PreTrainedKerasImageNetClassifier, site=site)

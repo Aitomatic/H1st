44a86147947775ea9b8655955ea96576f0c0d88b
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-09 12:13:45 -0700

    update h1st.django.modeling.scripts.setup_google_cloud_svc_models

--- h1st/django/modeling/scripts/setup_google_cloud_svc_models.py
+++ h1st/django/modeling/scripts/setup_google_cloud_svc_models.py
@@ -3,8 +3,10 @@ from ..models import (GoogleCloudTranslationServiceModel,
 
 
 def run():
-    print(GoogleCloudTranslationServiceModel.objects.get_or_create(
+    print(
+        GoogleCloudTranslationServiceModel.objects.get_or_create(
             name='Google-Cloud-Translation-Service-Model')[0])
 
-    print(GoogleTranslateServiceModel.objects.get_or_create(
+    print(
+        GoogleTranslateServiceModel.objects.get_or_create(
             name='Google-Translate-Service-Model')[0])

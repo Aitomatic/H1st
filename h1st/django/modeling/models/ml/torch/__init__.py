9c0abd253d7725bc12dcfb40e61e04841e8fcd40
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 03:46:45 -0700

    minor edit to h1st.django.modeling.models.ml.torch

--- h1st/django/modeling/models/ml/torch/__init__.py
+++ h1st/django/modeling/models/ml/torch/__init__.py
@@ -8,5 +8,4 @@ from .base import TorchModel, H1stTorchModel
 
 from .pre_trained.vision.image_classification import (
     PreTrainedTorchVisionImageNetClassifier,
-    H1stPreTrainedTorchVisionImageNetClassifier
-)
+    H1stPreTrainedTorchVisionImageNetClassifier)

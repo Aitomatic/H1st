c9a86c341310f540b4e88860a5ad0d0f5855c5e5
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-20 23:24:41 -0700

    add default h1st.django.modeling.models.base.Model.gradio_ui

--- h1st/django/modeling/models/base.py
+++ h1st/django/modeling/models/base.py
@@ -2,11 +2,14 @@ from json.decoder import JSONDecoder
 
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.fields.json import JSONField
+from django.utils.functional import classproperty
 
 from polymorphic.models import PolymorphicModel
 
 from django_plotly_dash import DjangoDash
 from gradio.interface import Interface
+from gradio.inputs import Dropdown as DropdownInputComponent
+from gradio.outputs import JSON as JSONOutputComponent
 
 from h1st.model.model import Model as CoreH1stModel
 
@@ -56,9 +59,154 @@ class Model(PolymorphicModel,
     def dash_ui(self) -> DjangoDash:
         return NotImplemented
 
-    @property
-    def gradio_ui(self) -> Interface:
-        return NotImplemented
+    @classproperty
+    def gradio_ui(cls) -> Interface:
+        model_names_or_uuids = cls.names_or_uuids
+
+        return Interface(
+            fn=lambda model_name_or_uuid, *args:
+                cls.get_by_name_or_uuid(name_or_uuid=model_name_or_uuid)
+                .predict(*args),
+            # (Callable) - the function to wrap an interface around.
+
+            inputs=[DropdownInputComponent(choices=model_names_or_uuids,
+                                           type='value',
+                                           default=model_names_or_uuids[0],
+                                           label='H1st Model Name or UUID'),
+                    ],
+            # (Union[str, List[Union[str, InputComponent]]]) -
+            # a single Gradio input component,
+            # or list of Gradio input components.
+            # Components can either be passed as instantiated objects,
+            # or referred to by their string shortcuts.
+            # The number of input components should match
+            # the number of parameters in fn.
+
+            outputs=JSONOutputComponent(label='H1st Model Output'),
+            # (Union[str, List[Union[str, OutputComponent]]]) -
+            # a single Gradio output component,
+            # or list of Gradio output components.
+            # Components can either be passed as instantiated objects,
+            # or referred to by their string shortcuts.
+            # The number of output components should match
+            # the number of values returned by fn.
+
+            verbose=True,
+            # (bool) - whether to print detailed information during launch.
+
+            examples=None,
+            # (Union[List[List[Any]], str]) - sample inputs for the function;
+            # if provided, appears below the UI components and can be used
+            # to populate the interface.
+            # Should be nested list, in which the outer list consists of
+            # samples and each inner list consists of an input
+            # corresponding to each input component.
+            # A string path to a directory of examples can also be provided.
+            # If there are multiple input components and a directory
+            # is provided, a log.csv file must be present in the directory
+            # to link corresponding inputs.
+
+            examples_per_page=10,
+            # (int) - If examples are provided, how many to display per page.
+
+            live=False,
+            # (bool) - should the interface automatically reload on change?
+
+            layout='unaligned',
+            # (str) - Layout of input and output panels.
+            # - "horizontal" arranges them as two columns of equal height,
+            # - "unaligned" arranges them as two columns of unequal height, and
+            # - "vertical" arranges them vertically.
+
+            show_input=True,
+            show_output=True,
+
+            capture_session=False,
+            # (bool) - if True, captures the default graph and session
+            # (needed for Tensorflow 1.x)
+
+            interpretation='default',
+            # (Union[Callable, str]) - function that provides interpretation
+            # explaining prediction output.
+            # Pass "default" to use built-in interpreter.
+
+            num_shap=2.0,
+            # (float) - a multiplier that determines how many examples
+            # are computed for shap-based interpretation.
+            # Increasing this value will increase shap runtime,
+            # but improve results.
+
+            theme='default',
+            # (str) - Theme to use - one of
+            # - "default",
+            # - "compact",
+            # - "huggingface", or
+            # - "darkhuggingface".
+
+            repeat_outputs_per_model=True,
+
+            title=cls._meta.verbose_name,
+            # (str) - a title for the interface;
+            # if provided, appears above the input and output components.
+
+            description='A Keras model trained to classify ImageNet data',
+            # (str) - a description for the interface;
+            # if provided, appears above the input and output components.
+
+            article=None,
+            # (str) - an expanded article explaining the interface;
+            # if provided, appears below the input and output components.
+            # Accepts Markdown and HTML content.
+
+            thumbnail=None,
+            # (str) - path to image or src to use as display picture for models
+            # listed in gradio.app/hub
+
+            css=None,
+            # (str) - custom css or path to custom css file
+            # to use with interface.
+
+            server_port=None,
+            # (int) - will start gradio app on this port (if available)
+
+            # server_name=networking.LOCALHOST_NAME,
+            # (str) - to make app accessible on local network set to "0.0.0.0".
+
+            height=500,
+            width=900,
+
+            allow_screenshot=True,
+            # (bool) - if False, users will not see a button
+            # to take a screenshot of the interface.
+
+            allow_flagging=True,
+            # (bool) - if False, users will not see a button
+            # to flag an input and output.
+
+            flagging_options=None,
+            # (List[str]) - if not None, provides options a user must select
+            # when flagging.
+
+            encrypt=False,
+            # (bool) - If True, flagged data will be encrypted
+            # by key provided by creator at launch
+
+            show_tips=False,
+            # (bool) - if True, will occasionally show tips
+            # about new Gradio features
+
+            embedding=None,
+
+            flagging_dir='flagged',
+            # (str) - what to name the dir where flagged data is stored.
+
+            analytics_enabled=True,
+
+            enable_queue=False,
+            # (bool) - if True, inference requests will be served through
+            # a queue instead of with parallel threads.
+            # Required for longer inference times (> 1min) to prevent timeout.
+        )
 
     @property
     def streamlit_ui(self):

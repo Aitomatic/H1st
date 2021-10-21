af5b24d9723f38265291649e247f613a6b75ae79
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 01:30:26 -0700

    refactor h1st.django.modeling.models.ml.hugging_face.translation.PreTrainedHuggingFaceTranslator

--- h1st/django/modeling/models/ml/hugging_face/translation.py
+++ h1st/django/modeling/models/ml/hugging_face/translation.py
@@ -1,7 +1,10 @@
 __all__ = ('PreTrainedHuggingFaceTranslator',
            'H1stPreTrainedHuggingFaceTranslator')
 
+
 from typing import Optional, Sequence, Union
+
+from django.utils.functional import classproperty
 
 from googletrans.constants import LANGUAGES
 
@@ -76,12 +79,12 @@ class PreTrainedHuggingFaceTranslator(PreTrainedHuggingFaceTransformer):
                     if single_text
                     else [result['translation_text'] for result in output])
 
-    @property
-    def gradio_ui(self) -> Interface:
+    @classproperty
+    def gradio_ui(cls) -> Interface:
         languages = tuple(LANGUAGES)
 
         return Interface(
-            fn=self.predict,
+            fn=cls.predict,
             # (Callable) - the function to wrap an interface around.
 
             inputs=[TextboxInputComponent(lines=10,
@@ -174,9 +177,11 @@ class PreTrainedHuggingFaceTranslator(PreTrainedHuggingFaceTransformer):
             theme='default',
             # (str) - Theme to use - one of
             # - "default",
-            # - "compact",
-            # - "huggingface", or
-            # - "darkhuggingface".
+            # - "huggingface",
+            # - "grass",
+            # - "peach".
+            # Add "dark" prefix, e.g. "darkpeach" or "darkdefault"
+            # for darktheme.
 
             repeat_outputs_per_model=True,
 

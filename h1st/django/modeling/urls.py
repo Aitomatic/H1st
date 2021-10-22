b1c2a1bac60f87abc99bfdfefe6b68fc75a26870
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 20:48:21 -0700

    refactor h1st.django.modeling.urls

--- h1st/django/modeling/urls.py
+++ h1st/django/modeling/urls.py
@@ -1,26 +1,32 @@
-from django.urls.conf import include, path
+from collections.abc import Sequence
 
-from rest_framework.routers import DefaultRouter
+from django.urls.conf import include, path
+from django.urls.resolvers import URLPattern
+
+from rest_framework.routers import BaseRouter, DefaultRouter
 
 from .api.rest.views import H1stModelViewSet, ModelExecAPIView
 from .views import launch_gradio_ui
 
 
-CORE_REST_API_ROUTER = DefaultRouter(trailing_slash=False)
-
-CORE_REST_API_ROUTER.register(
-    prefix='',
-    viewset=H1stModelViewSet,
-    basename=None)
+__all__: Sequence[str] = ('urlpatterns',)
 
 
-urlpatterns = (
+CORE_REST_ROUTER: BaseRouter = DefaultRouter(trailing_slash=False)
+
+CORE_REST_ROUTER.register(prefix='', viewset=H1stModelViewSet, basename=None)
+
+
+urlpatterns: Sequence[URLPattern] = (
     path(route='',
-         view=include(CORE_REST_API_ROUTER.urls)),
+         view=include(CORE_REST_ROUTER.urls),
+         kwargs=None, name=None),
 
     path(route='<str:model_name_or_uuid>/exec/',
-         view=ModelExecAPIView.as_view()),
+         view=ModelExecAPIView.as_view(),
+         kwargs=None, name=None),
 
     path(route='<str:model_class_or_instance_name_or_uuid>/gradio/',
-         view=launch_gradio_ui),
+         view=launch_gradio_ui,
+         kwargs=None, name=None),
 )

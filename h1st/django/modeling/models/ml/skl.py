dcd536056db95cbac9e238a0af717900c88ec12f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-22 00:02:51 -0700

    refactor h1st.django.modeling.models.ml.skl

--- h1st/django/modeling/models/ml/skl.py
+++ h1st/django/modeling/models/ml/skl.py
@@ -1,46 +1,45 @@
+from collections.abc import Sequence
+
+import joblib
+
 from ....util import PGSQL_IDENTIFIER_MAX_LEN
 from ...apps import H1stAIModelingModuleConfig
 from .base import H1stMLModel
 
 
+__all__: Sequence[str] = 'SKLModel', 'H1stSKLModel'
+
+
 class SKLModel(H1stMLModel):
     class Meta(H1stMLModel.Meta):
-        verbose_name = 'H1st SciKit-Learn Model'
-        verbose_name_plural = 'H1st SciKit-Learn Models'
+        verbose_name: str = 'H1st SciKit-Learn Model'
+        verbose_name_plural: str = 'H1st SciKit-Learn Models'
 
-        db_table = (f'{H1stAIModelingModuleConfig.label}_'
-                    f"{__qualname__.split('.')[0]}")
+        db_table: str = (f'{H1stAIModelingModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_skl_models'
+        default_related_name: str = 'h1st_skl_models'
 
     # by default, serialize model object by JobLib/Pickle
-    def serialize(self):
-        assert self.obj, ValueError(f'*** MODEL OBJECT {self.obj} INVALID ***')
+    def save(self, *args, **kwargs):
+        assert self.native_model_obj, \
+            ValueError(f'*** MODEL OBJECT {self.native_model_obj} INVALID ***')
 
-        with TemporaryFile() as f:
-            joblib.dump(
-                value=self.obj,
-                filename=f,
-                compress=0,
-                protocol=3,   # default protocol in Python 3.0–3.7
-                cache_size=None)
-            f.seek(0)
-            self.artifact = f.read()
+        joblib.dump(value=self.native_model_obj,
+                    filename=self.artifact_local_path,
+                    compress=0,
+                    protocol=3,   # default protocol in Python 3.0–3.7
+                    cache_size=None)
+
+        super().save(*args, **kwargs)
 
     # by default, deserialize model object by JobLib/Pickle
-    def deserialize(self):
-        with TemporaryFile() as f:
-            f.write(self.artifact)
-            f.seek(0)
-            self.obj = joblib.load(filename=f, mmap_mode=None)
-
-        assert self.obj, ValueError(f'*** MODEL OBJECT {self.obj} INVALID ***')
-
-    def save(self, *args, **kwargs):
-        self.serialize()
-        super().save(*args, **kwargs)
+    def load(self) -> None:
+        if self.native_model_obj is None:
+            self.native_model_obj = \
+                joblib.load(filename=self.artifact_local_path, mmap_mode=None)
 
 
 # alias

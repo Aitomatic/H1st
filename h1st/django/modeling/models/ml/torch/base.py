952b02ecc409883366c235970579fe59717fc643
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:25:27 -0700

    refactor h1st.django.modeling.models.ml.torch

--- h1st/django/modeling/models/ml/torch/base.py
+++ h1st/django/modeling/models/ml/torch/base.py
@@ -1,19 +1,24 @@
+from collections.abc import Sequence
+
 from .....util import PGSQL_IDENTIFIER_MAX_LEN
 from ....apps import H1stAIModelingModuleConfig
 from ..base import H1stMLModel
 
 
+__all__: Sequence[str] = 'TorchModel', 'H1stTorchModel'
+
+
 class TorchModel(H1stMLModel):
     class Meta(H1stMLModel.Meta):
-        verbose_name = 'H1st Torch Model'
-        verbose_name_plural = 'H1st Torch Models'
+        verbose_name: str = 'H1st Torch Model'
+        verbose_name_plural: str = 'H1st Torch Models'
 
-        db_table = (f'{H1stAIModelingModuleConfig.label}_'
-                    f"{__qualname__.split('.')[0]}")
+        db_table: str = (f'{H1stAIModelingModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_torch_models'
+        default_related_name: str = 'h1st_torch_models'
 
 
 # alias

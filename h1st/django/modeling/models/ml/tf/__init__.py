b378a408e2980596112fc4377c03fbdcd086a723
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:31:23 -0700

    refactor h1st.django.modeling.models.ml.tf

--- h1st/django/modeling/models/ml/tf/__init__.py
+++ h1st/django/modeling/models/ml/tf/__init__.py
@@ -1,10 +1,13 @@
-__all__ = ('TFModel', 'H1stTFModel',
-
-           'PreTrainedTFHubTransformer',
-           'H1stPreTrainedTFHubTransformer')
-
+from collections.abc import Sequence
 
 from .base import TFModel, H1stTFModel
 
 from .pre_trained.tf_hub import (PreTrainedTFHubTransformer,
                                  H1stPreTrainedTFHubTransformer)
+
+
+__all__: Sequence[str] = (
+    'TFModel', 'H1stTFModel',
+
+    'PreTrainedTFHubTransformer', 'H1stPreTrainedTFHubTransformer',
+)

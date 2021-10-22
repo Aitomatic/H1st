5b82617a54b1461fc42cf0d71f93a4d72f922078
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 21:12:42 -0700

    refactor h1st.django.modeling.models.cloud_svc.base

--- h1st/django/modeling/models/cloud_svc/base.py
+++ h1st/django/modeling/models/cloud_svc/base.py
@@ -1,19 +1,24 @@
+from collections.abc import Sequence
+
 from ....util import PGSQL_IDENTIFIER_MAX_LEN
 from ...apps import H1stAIModelingModuleConfig
 from ..base import H1stModel
 
 
+__all__: Sequence[str] = ('CloudServiceModel',)
+
+
 class CloudServiceModel(H1stModel):
     class Meta(H1stModel.Meta):
-        verbose_name = 'Cloud-Service Model'
-        verbose_name_plural = 'Cloud-Service Models'
+        verbose_name: str = 'Cloud-Service Model'
+        verbose_name_plural: str = 'Cloud-Service Models'
 
-        db_table = \
-            f"{H1stAIModelingModuleConfig.label}_{__qualname__.split('.')[0]}"
+        db_table: str = (f'{H1stAIModelingModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_cloud_svc_models'
+        default_related_name: str = 'h1st_cloud_svc_models'
 
 
 # alias

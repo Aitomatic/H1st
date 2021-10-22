ed1cccf6a1ec49d7955ac2234d47a1169761baee
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 21:23:49 -0700

    refactor h1st.django.modeling.models.graph

--- h1st/django/modeling/models/graph.py
+++ h1st/django/modeling/models/graph.py
@@ -1,4 +1,7 @@
 from abc import abstractmethod
+from collections.abc import Sequence
+
+from django.utils.functional import classproperty
 
 from h1st.h1flow.h1flow import Graph as CoreH1stWorkflow
 
@@ -8,21 +11,24 @@ from ..apps import H1stAIModelingModuleConfig
 from .base import H1stModel
 
 
+__all__: Sequence[str] = 'Graph', 'H1stGraph', 'Workflow', 'H1stWorkflow'
+
+
 class Workflow(H1stModel, CoreH1stWorkflow):
     class Meta(H1stModel.Meta):
-        verbose_name = 'H1st Workflow'
-        verbose_name_plural = 'H1st Workflows'
+        verbose_name: str = 'H1st Workflow'
+        verbose_name_plural: str = 'H1st Workflows'
 
-        db_table = (f'{H1stAIModelingModuleConfig.label}_'
-                    f"{__qualname__.split('.')[0]}")
+        db_table: str = (f'{H1stAIModelingModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_workflows'
+        default_related_name: str = 'h1st_workflows'
 
-    @property
+    @classproperty
     @abstractmethod
-    def args(self) -> tuple[str]:
+    def args(cls) -> Sequence[str]:
         raise NotImplementedError
 
     @enable_dict_io

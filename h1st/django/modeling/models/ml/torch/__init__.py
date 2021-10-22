723547f23af51e44c493021603673ba04fb21c33
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:19:19 -0700

    refactor h1st.django.modeling.models.ml.torch

--- h1st/django/modeling/models/ml/torch/__init__.py
+++ h1st/django/modeling/models/ml/torch/__init__.py
@@ -1,11 +1,13 @@
-__all__ = ('TorchModel', 'H1stTorchModel',
-
-           'PreTrainedTorchVisionImageNetClassifier',
-           'H1stPreTrainedTorchVisionImageNetClassifier')
-
+from collections.abc import Sequence
 
 from .base import TorchModel, H1stTorchModel
 
 from .pre_trained.vision.image_classification import (
     PreTrainedTorchVisionImageNetClassifier,
     H1stPreTrainedTorchVisionImageNetClassifier)
+
+
+__all__: Sequence[str] = ('TorchModel', 'H1stTorchModel',
+
+                          'PreTrainedTorchVisionImageNetClassifier',
+                          'H1stPreTrainedTorchVisionImageNetClassifier')

20706f77f68b2f4f606d8d6425df615a69f0b45f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:39:54 -0700

    refactor h1st.django.modeling.models.ml.keras

--- h1st/django/modeling/models/ml/keras/__init__.py
+++ h1st/django/modeling/models/ml/keras/__init__.py
@@ -1,10 +1,14 @@
-__all__ = ('KerasModel', 'H1stKerasModel',
-
-           'PreTrainedKerasImageNetClassifier',
-           'H1stPreTrainedKerasImageNetClassifier')
-
+from collections.abc import Sequence
 
 from .base import KerasModel, H1stKerasModel
 
-from .pre_trained.image_classification import \
-    PreTrainedKerasImageNetClassifier, H1stPreTrainedKerasImageNetClassifier
+from .pre_trained.image_classification import (
+    PreTrainedKerasImageNetClassifier, H1stPreTrainedKerasImageNetClassifier)
+
+
+__all__: Sequence[str] = (
+    'KerasModel', 'H1stKerasModel',
+
+    'PreTrainedKerasImageNetClassifier',
+    'H1stPreTrainedKerasImageNetClassifier',
+)

c05047a26900c98fe985378dc8272397bd02d61c
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:03:58 -0700

    refactor h1st.django.modeling.models.ml

--- h1st/django/modeling/models/ml/__init__.py
+++ h1st/django/modeling/models/ml/__init__.py
@@ -1,67 +1,4 @@
-__all__ = (
-    'MLModel', 'H1stMLModel',
-    'PyLoadablePreTrainedMLModel', 'H1stPyLoadablePreTrainedMLModel',
-
-    'SKLModel', 'H1stSKLModel',
-
-    'TFModel', 'H1stTFModel',
-    'PreTrainedTFHubTransformer', 'H1stPreTrainedTFHubTransformer',
-
-    'KerasModel', 'H1stKerasModel',
-
-    'PreTrainedKerasImageNetClassifier',
-    'H1stPreTrainedKerasImageNetClassifier',
-
-    'TorchModel', 'H1stTorchModel',
-
-    'PreTrainedTorchVisionImageNetClassifier',
-    'H1stPreTrainedTorchVisionImageNetClassifier',
-
-    'PreTrainedHuggingFaceTransformer', 'H1stPreTrainedHuggingFaceTransformer',
-
-    'PreTrainedHuggingFaceAudioClassifier',
-    'H1stPreTrainedHuggingFaceAudioClassifier',
-
-    'PreTrainedHuggingFaceImageClassifier',
-    'H1stPreTrainedHuggingFaceImageClassifier',
-
-    'PreTrainedHuggingFaceMaskFiller',
-    'H1stPreTrainedHuggingFaceMaskFiller',
-
-    'PreTrainedHuggingFaceObjectDetector',
-    'H1stPreTrainedHuggingFaceObjectDetector',
-
-    'PreTrainedHuggingFaceQuestionAnswerer',
-    'H1stPreTrainedHuggingFaceQuestionAnswerer',
-
-    'PreTrainedHuggingFaceSpeechRecognizer',
-    'H1stPreTrainedHuggingFaceSpeechRecognizer',
-
-    'PreTrainedHuggingFaceTableQuestionAnswerer',
-    'H1stPreTrainedHuggingFaceTableQuestionAnswerer',
-
-    'PreTrainedHuggingFaceTextClassifier',
-    'H1stPreTrainedHuggingFaceTextClassifier',
-
-    'PreTrainedHuggingFaceTextGenerator',
-    'H1stPreTrainedHuggingFaceTextGenerator',
-
-    'PreTrainedHuggingFaceText2TextGenerator',
-    'H1stPreTrainedHuggingFaceText2TextGenerator',
-
-    'PreTrainedHuggingFaceTextSummarizer',
-    'H1stPreTrainedHuggingFaceTextSummarizer',
-
-    'PreTrainedHuggingFaceTokenClassifier',
-    'H1stPreTrainedHuggingFaceTokenClassifier',
-
-    'PreTrainedHuggingFaceTranslator',
-    'H1stPreTrainedHuggingFaceTranslator',
-
-    'PreTrainedHuggingFaceZeroShotClassifier',
-    'H1stPreTrainedHuggingFaceZeroShotClassifier',
-)
-
+from collections.abc import Sequence
 
 from .base import (MLModel, H1stMLModel,
                    PyLoadablePreTrainedMLModel, H1stPyLoadablePreTrainedMLModel
@@ -129,4 +66,69 @@ from .hugging_face import (
 
     PreTrainedHuggingFaceZeroShotClassifier,
     H1stPreTrainedHuggingFaceZeroShotClassifier,
+)
+
+
+__all__: Sequence[str] = (
+    'MLModel', 'H1stMLModel',
+    'PyLoadablePreTrainedMLModel', 'H1stPyLoadablePreTrainedMLModel',
+
+    'SKLModel', 'H1stSKLModel',
+
+    'TFModel', 'H1stTFModel',
+    'PreTrainedTFHubTransformer', 'H1stPreTrainedTFHubTransformer',
+
+    'KerasModel', 'H1stKerasModel',
+
+    'PreTrainedKerasImageNetClassifier',
+    'H1stPreTrainedKerasImageNetClassifier',
+
+    'TorchModel', 'H1stTorchModel',
+
+    'PreTrainedTorchVisionImageNetClassifier',
+    'H1stPreTrainedTorchVisionImageNetClassifier',
+
+    'PreTrainedHuggingFaceTransformer', 'H1stPreTrainedHuggingFaceTransformer',
+
+    'PreTrainedHuggingFaceAudioClassifier',
+    'H1stPreTrainedHuggingFaceAudioClassifier',
+
+    'PreTrainedHuggingFaceImageClassifier',
+    'H1stPreTrainedHuggingFaceImageClassifier',
+
+    'PreTrainedHuggingFaceMaskFiller',
+    'H1stPreTrainedHuggingFaceMaskFiller',
+
+    'PreTrainedHuggingFaceObjectDetector',
+    'H1stPreTrainedHuggingFaceObjectDetector',
+
+    'PreTrainedHuggingFaceQuestionAnswerer',
+    'H1stPreTrainedHuggingFaceQuestionAnswerer',
+
+    'PreTrainedHuggingFaceSpeechRecognizer',
+    'H1stPreTrainedHuggingFaceSpeechRecognizer',
+
+    'PreTrainedHuggingFaceTableQuestionAnswerer',
+    'H1stPreTrainedHuggingFaceTableQuestionAnswerer',
+
+    'PreTrainedHuggingFaceTextClassifier',
+    'H1stPreTrainedHuggingFaceTextClassifier',
+
+    'PreTrainedHuggingFaceTextGenerator',
+    'H1stPreTrainedHuggingFaceTextGenerator',
+
+    'PreTrainedHuggingFaceText2TextGenerator',
+    'H1stPreTrainedHuggingFaceText2TextGenerator',
+
+    'PreTrainedHuggingFaceTextSummarizer',
+    'H1stPreTrainedHuggingFaceTextSummarizer',
+
+    'PreTrainedHuggingFaceTokenClassifier',
+    'H1stPreTrainedHuggingFaceTokenClassifier',
+
+    'PreTrainedHuggingFaceTranslator',
+    'H1stPreTrainedHuggingFaceTranslator',
+
+    'PreTrainedHuggingFaceZeroShotClassifier',
+    'H1stPreTrainedHuggingFaceZeroShotClassifier',
 )

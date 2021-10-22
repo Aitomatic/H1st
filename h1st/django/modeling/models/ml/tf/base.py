fa31584a89bc8ff3dbfe34b2dbee2425845cbd41
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 22:30:30 -0700

    refactor h1st.django.modeling.models.ml.tf.base

--- h1st/django/modeling/models/ml/tf/base.py
+++ h1st/django/modeling/models/ml/tf/base.py
@@ -1,22 +1,24 @@
-__all__ = 'TFModel', 'H1stTFModel'
-
+from collections.abc import Sequence
 
 from .....util import PGSQL_IDENTIFIER_MAX_LEN
 from ....apps import H1stAIModelingModuleConfig
 from ..base import H1stMLModel
 
 
+__all__: Sequence[str] = 'TFModel', 'H1stTFModel'
+
+
 class TFModel(H1stMLModel):
     class Meta(H1stMLModel.Meta):
-        verbose_name = 'H1st TensorFlow Model'
-        verbose_name_plural = 'H1st TensorFlow Models'
+        verbose_name: str = 'H1st TensorFlow Model'
+        verbose_name_plural: str = 'H1st TensorFlow Models'
 
-        db_table = (f'{H1stAIModelingModuleConfig.label}_'
-                    f"{__qualname__.split('.')[0]}")
+        db_table: str = (f'{H1stAIModelingModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'h1st_tf_models'
+        default_related_name: str = 'h1st_tf_models'
 
 
 # alias

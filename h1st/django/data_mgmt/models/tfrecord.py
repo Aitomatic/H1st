6c221d18b05f37778c1756bb78b8c3067a438c7f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 21:51:00 -0700

    refactor h1st.django.data_mgmt.models.tfrecord

--- h1st/django/data_mgmt/models/tfrecord.py
+++ h1st/django/data_mgmt/models/tfrecord.py
@@ -1,16 +1,21 @@
+from collections.abc import Sequence
+
 from ...util import PGSQL_IDENTIFIER_MAX_LEN
 from ..apps import H1stAIDataManagementModuleConfig
 from .base import DataSet
 
 
+__all__: Sequence[str] = ('TFRecordDataSet',)
+
+
 class TFRecordDataSet(DataSet):
     class Meta(DataSet.Meta):
-        verbose_name = 'TFRecord Data Set'
-        verbose_name_plural = 'TFRecord Data Sets'
+        verbose_name: str = 'TFRecord Data Set'
+        verbose_name_plural: str = 'TFRecord Data Sets'
 
-        db_table = (f'{H1stAIDataManagementModuleConfig.label}_'
-                    f"{__qualname__.split('.')[0]}")
+        db_table: str = (f'{H1stAIDataManagementModuleConfig.label}_'
+                         f"{__qualname__.split(sep='.', maxsplit=1)[0]}")
         assert len(db_table) <= PGSQL_IDENTIFIER_MAX_LEN, \
             ValueError(f'*** "{db_table}" DB TABLE NAME TOO LONG ***')
 
-        default_related_name = 'tfrecord_data_sets'
+        default_related_name: str = 'tfrecord_data_sets'

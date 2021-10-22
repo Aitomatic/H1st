84c45f49a01a3523a12b6f140b8e7036ac7d5ce7
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-21 20:50:53 -0700

    refactor h1st.django.data_mgmt.urls

--- h1st/django/data_mgmt/urls.py
+++ h1st/django/data_mgmt/urls.py
@@ -1,22 +1,27 @@
+from collections.abc import Sequence
+
 from django.urls.conf import include, path
+from django.urls.resolvers import URLPattern
 
 from rest_framework.routers import DefaultRouter
 
 from .api.rest.views import DataSetViewSet, DataQueryAPIView
 
 
+__all__: Sequence[str] = ('urlpatterns',)
+
+
 CORE_REST_API_ROUTER = DefaultRouter(trailing_slash=False)
 
-CORE_REST_API_ROUTER.register(
-    prefix='',
-    viewset=DataSetViewSet,
-    basename=None)
+CORE_REST_API_ROUTER.register(prefix='', viewset=DataSetViewSet, basename=None)
 
 
-urlpatterns = (
+urlpatterns: Sequence[URLPattern] = (
     path(route='',
-         view=include(CORE_REST_API_ROUTER.urls)),
+         view=include(CORE_REST_API_ROUTER.urls),
+         kwargs=None, name=None),
 
     path(route='query/',
-         view=DataQueryAPIView.as_view()),
+         view=DataQueryAPIView.as_view(),
+         kwargs=None, name=None),
 )

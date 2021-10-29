e41f34b90b811d8602038670153a1c68ff985eb5
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-10-29 10:08:01 -0700

    refactor h1st.django.util.cli

--- h1st/django/util/cli/run_command.py
+++ h1st/django/util/cli/run_command.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import shutil
 from typing import Optional
 
-from ..config import _H1ST_DJANGO_CONFIG_FILE_NAME, parse_config_file
 from ..git import _GIT_HASH_FILE_NAME, get_git_repo_head_commit_hash
 
 
@@ -21,23 +20,10 @@ _WSGI_PY_FILE_SRC_PATH = \
 _PROCFILE_NAME = 'Procfile'
 
 
-def run_command_with_config_file(
-        command: str,
-        h1st_django_config_file_path: str,
-        copy_standard_files: bool = False,
-        asgi: Optional[str] = None):
-    h1st_django_config_file_path = \
-        Path(h1st_django_config_file_path).expanduser()
-
-    # verify config file is valid
-    config = parse_config_file(path=h1st_django_config_file_path)
-
+def run_command(command: str,
+                copy_standard_files: bool = False,
+                asgi: Optional[str] = None):
     if copy_standard_files:
-        assert not os.path.exists(path=_H1ST_DJANGO_CONFIG_FILE_NAME)
-        shutil.copyfile(
-            src=h1st_django_config_file_path,
-            dst=_H1ST_DJANGO_CONFIG_FILE_NAME)
-
         if asgi:
             assert not os.path.exists(path=_ASGI_PY_FILE_NAME)
             shutil.copyfile(
@@ -45,8 +31,8 @@ def run_command_with_config_file(
                 dst=_ASGI_PY_FILE_NAME)
             assert not os.path.exists(path=_PROCFILE_NAME)
             shutil.copyfile(
-                src=_H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH /
-                    f'{_PROCFILE_NAME}.{asgi.capitalize()}',
+                src=(_H1ST_DJANGO_UTIL_CLI_STANDARD_FILES_DIR_PATH /
+                     f'{_PROCFILE_NAME}.{asgi.capitalize()}'),
                 dst=_PROCFILE_NAME)
         else:
             assert not os.path.exists(path=_WSGI_PY_FILE_NAME)
@@ -60,20 +46,7 @@ def run_command_with_config_file(
             with open(_GIT_HASH_FILE_NAME, 'w') as f:
                 f.write(git_hash)
 
-    aws_config = config.get('aws')
-    if aws_config:
-        key = aws_config.get('key')
-        secret = aws_config.get('secret')
-        if key and secret:
-            os.environ.setdefault('AWS_ACCESS_KEY_ID', key)
-            os.environ.setdefault('AWS_SECRET_ACCESS_KEY', secret)
-
-    print(f'Running Command: {command}...')
-    os.system(
-        (''
-         if copy_standard_files
-         else f'H1ST_DJANGO_CONFIG_FILE_PATH={h1st_django_config_file_path} ')
-        + command)
+    os.system(command=command)
 
     if copy_standard_files:
         os.remove(_H1ST_DJANGO_CONFIG_FILE_NAME)

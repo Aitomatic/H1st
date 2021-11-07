52fd13278ec37c6eea744735e951c654eea7f2b3
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-11-07 01:24:01 -0800

    minor edit to h1st.__version__

--- h1st/__init__.py
+++ h1st/__init__.py
@@ -8,4 +8,4 @@ from typing import Sequence   # TODO: Py3.9: use generics/collections.abc
 __all__: Sequence[str] = ('__version__',)
 
 
-__version__ = version(distribution_name='H1st')
+__version__: str = version(distribution_name='H1st')

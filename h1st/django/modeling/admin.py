4d24b195b1f23da9339e3c95c05ccf610a0efecb
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-11-25 16:22:22 -0800

    update deps

--- h1st/django/modeling/admin.py
+++ h1st/django/modeling/admin.py
@@ -1,126 +1,111 @@
 """H1st Django Modeling admin."""
 
 
-from django.contrib.admin.decorators import register
-from django.contrib.admin.options import ModelAdmin
-from django.contrib.admin.sites import site
+# from django.contrib.admin.decorators import register
+# from django.contrib.admin.options import ModelAdmin
+# from django.contrib.admin.sites import site
 
-from silk.profiling.profiler import silk_profile
+# from silk.profiling.profiler import silk_profile
 
-from .models import (
-    KnowledgeModel,
-
-    GoogleCloudTranslationServiceModel, GoogleTranslateServiceModel,
-    PreTrainedKerasImageNetClassifier, PreTrainedTorchVisionImageNetClassifier,
-    PreTrainedHuggingFaceTransformer,
-)
-
-
-class H1stModelAdmin(ModelAdmin):
-    readonly_fields = ('gradio_interactive_ui',)
-
-    def gradio_interactive_ui(self, obj):
-        return ('(not implemented)'
-                if obj.gradio_ui is NotImplemented
-                else f'h1st/models/{obj.name_or_uuid}/ui')
+# from .models import (
+#     GoogleCloudTranslationServiceModel, GoogleTranslateServiceModel,
+#     PreTrainedKerasImageNetClassifier,
+#     PreTrainedTorchVisionImageNetClassifier,
+#     PreTrainedHuggingFaceTransformer,
+# )
 
 
-@register(KnowledgeModel, site=site)
-class KnowledgeModelAdmin(H1stModelAdmin):
-    show_full_result_count = False
+# class H1stModelAdmin(ModelAdmin):
+#     readonly_fields = ('gradio_interactive_ui',)
 
-    @silk_profile(name=(f'{__module__}: {KnowledgeModel._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
-
-    @silk_profile(
-        name=(f'{__module__}: {KnowledgeModel._meta.verbose_name_plural}'))
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
+#     def gradio_interactive_ui(self, obj):
+#         return ('(not implemented)'
+#                 if obj.gradio_ui is NotImplemented
+#                 else f'h1st/models/{obj.name_or_uuid}/gradio')
 
 
-@register(GoogleCloudTranslationServiceModel, site=site)
-class GoogleCloudTranslationServiceModelAdmin(H1stModelAdmin):
-    show_full_result_count = False
+# @register(GoogleCloudTranslationServiceModel, site=site)
+# class GoogleCloudTranslationServiceModelAdmin(H1stModelAdmin):
+#     show_full_result_count = False
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{GoogleCloudTranslationServiceModel._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{GoogleCloudTranslationServiceModel._meta.verbose_name}'))
+#     def changeform_view(self, *args, **kwargs):
+#         return super().changeform_view(*args, **kwargs)
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{GoogleCloudTranslationServiceModel._meta.verbose_name_plural}'
-              ))
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
-
-
-@register(GoogleTranslateServiceModel, site=site)
-class GoogleTranslateServiceModelAdmin(H1stModelAdmin):
-    show_full_result_count = False
-
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{GoogleTranslateServiceModel._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
-
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{GoogleTranslateServiceModel._meta.verbose_name_plural}'))
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{GoogleCloudTranslationServiceModel._meta.verbose_name_plural}'
+#               ))
+#     def changelist_view(self, *args, **kwargs):
+#         return super().changelist_view(*args, **kwargs)
 
 
-@register(PreTrainedKerasImageNetClassifier, site=site)
-class PreTrainedKerasImageNetClassifierAdmin(H1stModelAdmin):
-    show_full_result_count = False
+# @register(GoogleTranslateServiceModel, site=site)
+# class GoogleTranslateServiceModelAdmin(H1stModelAdmin):
+#     show_full_result_count = False
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{PreTrainedKerasImageNetClassifier._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{GoogleTranslateServiceModel._meta.verbose_name}'))
+#     def changeform_view(self, *args, **kwargs):
+#         return super().changeform_view(*args, **kwargs)
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{PreTrainedKerasImageNetClassifier._meta.verbose_name_plural}')
-    )
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
-
-
-@register(PreTrainedTorchVisionImageNetClassifier, site=site)
-class PreTrainedTorchVisionImageNetClassifierAdmin(H1stModelAdmin):
-    show_full_result_count = False
-
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{PreTrainedTorchVisionImageNetClassifier._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
-
-    @silk_profile(
-        f'{__module__}: '
-        f'{PreTrainedTorchVisionImageNetClassifier._meta.verbose_name_plural}')
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{GoogleTranslateServiceModel._meta.verbose_name_plural}'))
+#     def changelist_view(self, *args, **kwargs):
+#         return super().changelist_view(*args, **kwargs)
 
 
-@register(PreTrainedHuggingFaceTransformer, site=site)
-class PreTrainedHuggingFaceTransformerAdmin(H1stModelAdmin):
-    show_full_result_count = False
+# @register(PreTrainedKerasImageNetClassifier, site=site)
+# class PreTrainedKerasImageNetClassifierAdmin(H1stModelAdmin):
+#     show_full_result_count = False
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{PreTrainedHuggingFaceTransformer._meta.verbose_name}'))
-    def changeform_view(self, *args, **kwargs):
-        return super().changeform_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{PreTrainedKerasImageNetClassifier._meta.verbose_name}'))
+#     def changeform_view(self, *args, **kwargs):
+#         return super().changeform_view(*args, **kwargs)
 
-    @silk_profile(
-        name=(f'{__module__}: '
-              f'{PreTrainedHuggingFaceTransformer._meta.verbose_name_plural}'))
-    def changelist_view(self, *args, **kwargs):
-        return super().changelist_view(*args, **kwargs)
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{PreTrainedKerasImageNetClassifier._meta.verbose_name_plural}')
+#     )
+#     def changelist_view(self, *args, **kwargs):
+#         return super().changelist_view(*args, **kwargs)
+
+
+# @register(PreTrainedTorchVisionImageNetClassifier, site=site)
+# class PreTrainedTorchVisionImageNetClassifierAdmin(H1stModelAdmin):
+#     show_full_result_count = False
+
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{PreTrainedTorchVisionImageNetClassifier._meta.verbose_name}'))
+#     def changeform_view(self, *args, **kwargs):
+#         return super().changeform_view(*args, **kwargs)
+
+#     @silk_profile(
+#         f'{__module__}: '
+#         f'{PreTrainedTorchVisionImageNetClassifier._meta.verbose_name_plural}')
+#     def changelist_view(self, *args, **kwargs):
+#         return super().changelist_view(*args, **kwargs)
+
+
+# @register(PreTrainedHuggingFaceTransformer, site=site)
+# class PreTrainedHuggingFaceTransformerAdmin(H1stModelAdmin):
+#     show_full_result_count = False
+
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{PreTrainedHuggingFaceTransformer._meta.verbose_name}'))
+#     def changeform_view(self, *args, **kwargs):
+#         return super().changeform_view(*args, **kwargs)
+
+#     @silk_profile(
+#         name=(f'{__module__}: '
+#               f'{PreTrainedHuggingFaceTransformer._meta.verbose_name_plural}'))
+#     def changelist_view(self, *args, **kwargs):
+#         return super().changelist_view(*args, **kwargs)
